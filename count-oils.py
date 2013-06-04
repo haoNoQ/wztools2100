c4a4f6e20ffe8f359666e69469047d3735c060ba
haonoq <noqnoqneo@gmail.com>
2013-06-04 08:51:20 +0000

    count-oils.py: don't unpack the zip archive, fix indent

--- count-oils.py
+++ count-oils.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python2
 
 # usage:
-#   	count-oils.py 2c-SomeMap.wz
+#     count-oils.py 2c-SomeMap.wz
 
 # Counts oil resources on the map. This includes oils under derricks and
 # unoccupied oils, which sometimes may overlap, hence the complexity.
@@ -9,114 +9,101 @@
 
 import sys
 import zipfile
-import os.path
-import tempfile
 
 mapfile = sys.argv[1]
-zipfile = zipfile.ZipFile(mapfile)
-tempdir = tempfile.mkdtemp()
+wzfile = zipfile.ZipFile(mapfile)
 oillist = []
 
-# gah, i do it in one line in bash
-def unzip(wzfile, directory):
-  for path in wzfile.namelist():
-    clean_path = path.replace('\\', '/')  # no need if/else
-    dirname, filename = os.path.split(clean_path)  # unused parenthesis
-    if dirname:  # "" evaluates to False
-      dirname = os.path.join(directory, dirname)
-      if not os.path.exists(dirname):
-        os.mkdir(dirname)
-    else:
-      dirname = directory
-    if filename:
-      with open(os.path.join(directory, clean_path), "w") as fd:  # with statement is recommended to use with open
-        fd.write(wzfile.read(path))
+def findfile(filename):
+    global wzfile
+    for path in wzfile.namelist():
+        if (path.endswith(filename)):
+            return wzfile.open(path, "r")
 
+def add_to_oillist(x, y):
+    global oillist
+    oillist.append(str(x) + " " + str(y))
 
-# one more useful command
-def findfile(filename, directory):
-  for root, dirs, files in os.walk(directory):
-    if filename in files:
-      return os.path.join(root, filename)
-  #  return "" # if not return result will be returned None
+def safe_close(fd):
+    try:
+      fd.close()
+    except:
+      return
 
-def add_to_list(x, y):
-  # list dic, object are mutable, no need to declare global to mute them.
-  oillist.append("%s %s" % (x, y))  # use string formating
+def read_ini_sections(fd):
+    ret = {}
+    try:
+        line = fd.readline()
+    except:
+        return ret
+    nm = ""
+    while line != "":
+        if '[' in line:
+            nm = line.strip()[1:-1]
+            ret[nm] = {}
+        else:
+            try:
+                key, value = line.split('=')
+            except:
+                line = fd.readline()
+                continue
+            ret[nm][key.strip()] = value.strip()
+        line = fd.readline()
+    return ret
 
-# parse feature.ini, take oil resource positions, add them to the list
-def parse_feature_ini(filename):
-  if filename:
-    return
-  with open(filename, "r") as fd:
-    l1, l2, l3 = "", "", ""
-    for line in fd:
-      l3 = l2
-      l2 = l1
-      l1 = line
-      if "OilResource" in l1:
-        x, y = l3.split()[2:4]
-        x, y = int(x[:-1]) / 128, int(y[:-1]) / 128
-        add_to_list(x, y)
+def parse_feature_ini(fd):
+    sections = read_ini_sections(fd)
+    for sname, section in sections.iteritems():
+        if section["name"] == "OilResource":
+            x, y = section["position"].replace(',', '').split()[0:2]
+            add_to_oillist(int(x) / 128, int(y) / 128)
+    safe_close(fd)
 
+def parse_struct_ini(fd):
+    sections = read_ini_sections(fd)
+    for sname, section in sections.iteritems():
+        if section["name"] == "A0ResourceExtractor":
+            x, y = section["position"].replace(',', '').split()[0:2]
+            add_to_oillist(int(x) / 128, int(y) / 128)
+    safe_close(fd)
 
-# parse struct.ini, take oil resource positions, add them to the list
-def parse_struct_ini(filename):
-  if filename:
-    return
-  with open(filename, "r") as fd:
-    for line in fd:
-      if "A0ResourceExtractor" in line:
-        x, y = fd.next().split()[2:4]
-        x = int(x[:-1]) / 128
-        y = int(y[:-1]) / 128
-        add_to_list(x, y)
-
-
-# parse feat.bjo, take oil resource positions, add them to the list
-def parse_feat_bjo(filename):
-  if filename:
-    return
-  with open(filename, "r") as fd:
-    fd.read(12)
+def parse_feat_bjo(fd):
+    try:
+        fd.read(12)
+    except:
+        return
     while True:
-      s = fd.read(76)
-      if s == "":
-        break
-      if not s.startswith("OilResource"):
-        continue
-      s = s[44:50]
-      x = (ord(s[1]) * 256 + ord(s[0])) / 128
-      y = (ord(s[5]) * 256 + ord(s[4])) / 128
-      add_to_list(x, y)
+        s = fd.read(76)
+        if s == "":
+            break
+        if not s.startswith("OilResource"):
+            continue
+        s = s[44:50]
+        x = (ord(s[1]) * 256 + ord(s[0])) / 128
+        y = (ord(s[5]) * 256 + ord(s[4])) / 128
+        add_to_oillist(x, y) 
+    safe_close(fd)
 
-
-# parse struct.bjo, take oil resource positions, add them to the list
-def parse_struct_bjo(filename):
-  if filename:
-    return
-  with open(filename, "r") as fd:
-    fd.read(12)
+def parse_struct_bjo(fd):
+    try:
+        fd.read(12)
+    except:
+        return
     while True:
-      s = fd.read(120)
-      if s == "":
-        break
-      if not s.startswith("A0ResourceExtractor"):
-        continue
-      s = s[44:50]
-      x = (ord(s[1]) * 256 + ord(s[0])) / 128
-      y = (ord(s[5]) * 256 + ord(s[4])) / 128
-      add_to_list(x, y)
+        s = fd.read(120)
+        if s == "":
+            break
+        if not s.startswith("A0ResourceExtractor"):
+            continue
+        s = s[44:50]
+        x = (ord(s[1]) * 256 + ord(s[0])) / 128
+        y = (ord(s[5]) * 256 + ord(s[4])) / 128
+        add_to_oillist(x, y)
+    safe_close(fd)
 
-# here goes nothing why?
-
-if __name__ == '__main__':  # always use it for sripts with side effects.
-    unzip(zipfile, tempdir)
-
-    parse_feature_ini(findfile("feature.ini", tempdir))
-    parse_struct_ini(findfile("struct.ini", tempdir))
-    parse_feat_bjo(findfile("feat.bjo", tempdir))
-    parse_struct_bjo(findfile("struct.bjo", tempdir))
-
-    print len(set(oillist))
-
+if __name__ == "__main__":
+    parse_feature_ini(findfile("feature.ini"))
+    parse_struct_ini(findfile("struct.ini"))
+    parse_feat_bjo(findfile("feat.bjo"))
+    parse_struct_bjo(findfile("struct.bjo"))
+    print len(list(set(oillist)))

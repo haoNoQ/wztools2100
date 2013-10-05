0b956abeaa73d6fe5ecd2279211da5eeed97ee41
haonoq <noqnoqneo@gmail.com>
2013-10-05 18:58:41 +0400

    csv2ini.py: don't remove '*' from names, mostly to clean up ini-diff test script spam

--- csv2ini.py
+++ csv2ini.py
@@ -130,9 +130,9 @@ def load_messages_strings_names_txt():
 	strlist = remove_c_style_comments(fd)
 	fd.close()
 	if sys.hexversion >= 0x03000000:
-		trans = str.maketrans("_()\"*", "     ")
+		trans = str.maketrans("_()\"", "    ")
 	else:
-		trans = string.maketrans("_()\"*", "     ")
+		trans = string.maketrans("_()\"", "    ")
 	for line in strlist:
 		one,two = line.split(None, 1)
 		messages_strings_names_txt[one] = two.translate(trans).strip()

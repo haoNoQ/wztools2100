603a180d0e85e64c00a6a50d2e1e123a2772ef6b
Cjkjvfnby <Cjkjvfnby@gmail.com>
2013-10-06 12:21:10 +0400

    add command line options

--- ini_tools/validate_stats.py
+++ ini_tools/validate_stats.py
@@ -1,9 +1,17 @@
 import os
 from ini_file import IniFile, WZException
-
-stats_paths = ['G:\\warzone2100\\data\\base\\stats', 'G:\\warzone2100\\data\\mp\\stats']
+import argparse
 
 if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description="Usage: %prog [stats_folder, ....]")
+    parser.add_argument('stats_folders', help='folder with ini files', nargs='+')
+    args = parser.parse_args()
+
+    stats_paths = args.stats_folders
+    if not all([os.path.exists(path) for path in stats_paths]):
+        print "Invalid paths:", [path for path in stats_paths if not os.path.exists(path)]
+        exit(1)
+
     for stats_dir in stats_paths:
         files = [path for path in os.listdir(stats_dir) if path.endswith('.ini')]
         for path in files:

7c9e980b6f7aa09083f0d69ed74685b12c1a25a7
Cjkjvfnby <Cjkjvfnby@gmail.com>
2013-10-13 17:06:15 +0400

    add enviroment file

---
+++ enviroment.py
@@ -0,0 +1,12 @@
+import os
+try:
+    import settings
+except ImportError:
+    print "You must create settings file "
+
+REPO_PATH = settings.REPO_PATH
+assert os.path.exists(REPO_PATH), '%s does not exists' % REPO_PATH
+
+BASE_PATH = getattr(settings, "BASE_PATH", default=os.path.join(REPO_PATH, 'data', 'base'))
+MP_PATH = getattr(settings, "MP_PATH", default=os.path.join(REPO_PATH, 'data', 'mp'))
+MODS_PATH = getattr(settings, "MODS_PATH", default=[])
---
+++ settings_sample.py
@@ -0,0 +1,4 @@
+# path to warzone2100 repo
+REPO_PATH = ''
+# windows example
+#REPO_PATH = 'G:/warzone2100/'

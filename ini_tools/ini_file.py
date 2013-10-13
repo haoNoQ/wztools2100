413eb8cc0b7a8a70085571d376ffbb72a1d094c0
Cjkjvfnby <Cjkjvfnby@gmail.com>
2013-10-14 01:26:34 +0400

    add save function to IniFile

--- ini_tools/ini_file.py
+++ ini_tools/ini_file.py
@@ -30,3 +30,30 @@ class IniFile(dict):
         config.load(path)
         for section_name in config.sections():
             self[section_name] = dict(config.items(section_name))
+
+    def save(self, fd):
+        text_list = [self.profile.get_header()]
+        for section_name, section_items in self.items():
+            section_list = ['', '[%s]' % section_name]
+            for item in sorted(section_items.items(),  key=lambda x: self.profile.field_order.index(x[0])):
+                print item
+                prepared_value = self.prepare_value(item)
+                if prepared_value:
+                    section_list.append(prepared_value)
+            text_list.extend(section_list)
+        fd.write('\n'.join(text_list))
+
+    def prepare_value(self, item):
+        key, val = item
+        field = self.profile[key]
+        if str(field.get('default')) == val:
+            return None
+        if field['type'] == 'pie':
+            return "%s = %s" % (key, val.lower())
+        return "%s = %s" % item
+
+
+#if __name__ == '__main__':
+#    ini_file = IniFile("G:/warzone2100/data/base/stats/propulsion.ini")
+#    with open('tmp.ini', 'w') as fd:
+#        ini_file.save(fd)

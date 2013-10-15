ba9ebfaf7e02b589730141b2ddcbe3f64024c0c8
Cjkjvfnby <Cjkjvfnby@gmail.com>
2013-10-16 03:00:47 +0400

    move generate header from profile_loader

--- ini_tools/generate_ini_header.py
+++ ini_tools/generate_ini_header.py
@@ -1,5 +1,54 @@
 from profile_loader import get_profiles
 
+MAX_ITEM_TYPE_LENGTH = 40
+
+def get_header(profile):
+    result = [
+        "; Section headers in brackets [...] is system ID of %s items. This IDs should be unique." % profile.file_name,
+        '; Following properties can be used in %s items:' % profile.file_name]
+
+    fields = profile.keys()
+    fields.sort(key=lambda x: profile.field_order.index(x))
+    lines = []
+    for key, item in profile.items():
+        convert_to = item['convert_to']
+        item_type = item['type']
+        help_text = item.get('help_text', 'unknown!!!')
+        name = item['name']
+
+        value_type = item_type
+
+        if item_type in ('choice', 'boolean'):
+            text = '|'.join(str(x) for x in item['choices'])
+            if len(text) < MAX_ITEM_TYPE_LENGTH - 2:
+                value_type = text
+            else:
+                help_text += ' [%s]' % ', '.join([str(x) for x in item['choices']])
+        elif item_type == 'key':
+            value_type = 'ID for %s' % item['reference']
+        elif item_type == 'key_list':
+            value_type = 'comma-separated list of %s IDs' % item['reference']
+        elif convert_to == 'string_list':
+            value_type = 'comma-separated list'
+        elif convert_to == 'pie':
+            value_type = 'comma-separated pies'
+        elif convert_to == 'int':
+            value_type = 'integer'
+        elif convert_to == "str":
+            value_type = 'string'
+
+        value_type = '<%s>' % value_type
+        lines.append((name, value_type, help_text))
+
+    len_name = max([len(x[0]) for x in lines])
+    len_type = min(max([len(x[1]) for x in lines]), MAX_ITEM_TYPE_LENGTH)
+
+    template = ';  %-{0}s   %-{1}s  %s'.format(len_name, len_type)
+
+    result += (template % x for x in lines)
+    return '\n'.join(result)
+
+
 for profile in get_profiles():
     print "\n" * 3
-    print profile.get_header()
+    print get_header(profile)
--- ini_tools/profile_loader.py
+++ ini_tools/profile_loader.py
@@ -33,49 +33,6 @@ class Profile(dict):
             profile.update(**field)
             self[field['name']] = profile
 
-    def get_header(self):
-        result = [";Section headers in brackets [...] is system ID of %s items. This IDs should be unique." % self.file_name,
-                  ';Following properties can be used in %s items:' % self.file_name]
-
-        fields = self.keys()
-        fields.sort(key=lambda x: self.field_order.index(x))
-        lines = [self._get_field_header_line(key) for key in fields]
-
-        len_name = max([len(x[0]) for x in lines])
-        len_type = min(max([len(x[1]) for x in lines]), 40)
-
-        template = ';  %-{0}s   %-{1}s  %s'.format(len_name, len_type)
-
-        result += (template % x for x in lines)
-        return '\n'.join(result)
-
-    def _get_field_value_type(self, item):
-        convert_to = item['convert_to']
-        item_type = item['type']
-        if item_type == 'choice':
-            return 'one of: %s' % '|'.join([str(x) for x in item['choices']])
-
-        if item_type == 'key':
-            return 'ID for %s' % item['reference']
-        if item_type == 'key_list':
-            return 'comma-separated list of %s IDs' % item['reference']
-        if convert_to == 'string_list':
-            return 'comma-separated list'
-        if convert_to == 'boolean':
-            return '1 - True,  2 - False'
-        if convert_to == 'pie':
-            return 'comma-separated pie file names'
-        if convert_to == 'int':
-            return 'integer'
-        if convert_to == "str":
-            return 'string'
-        return convert_to
-
-    def _get_field_header_line(self, key):
-        item = self[key]
-        value_type = '<%s>' % self._get_field_value_type(item)
-        return item['name'], value_type, item['help_text']
-
 
 def get_profiles_name_list():
     return [name[:-5] for name in os.listdir(os.path.join(this_dir, '../ini_tools/profile')) if name.endswith('.json')]
